73052afa395996227e1c37d49f89fa9254d8d27e
Eric Leung <etleung21@gmail.com>
2015-08-09 20:09:45 -0700

    add learning c the hard way progress

---
+++ c-lcthw/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// File: ex1.c
+// Purpose: Prints out "Hello World."
+
+int main( int argc, char *argv[])
+{
+    puts("Hello World.");
+
+    return 0;
+}
---
+++ c-lcthw/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// File: ex3.c
+// Purpose: Print out integers using formatted strings
+
+int main() {
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches talls.\n", height);
+
+    return 0;
+}
