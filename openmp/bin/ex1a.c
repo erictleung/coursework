18e80f89ed08e713f3b3cd59aa091ed8e01f8dbd
Eric Leung <etleung21@gmail.com>
2015-08-19 23:01:02 -0700

    add first exercise programs

---
+++ openmp/bin/ex1a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Exercise 1A: Hello World
+// This is to demonstrate a simple "Hello World" statement in C
+
+int main()
+{
+    int ID = 0;
+    printf("hello(%d)", ID);
+    printf("world(%d)\n", ID);
+}
---
+++ openmp/bin/ex1b.c
@@ -0,0 +1,12 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main() 
+{
+    #pragma omp parallel
+    {
+    int ID = 0;
+    printf("hello(%d)", ID);
+    printf("world(%d)", ID);
+    }
+}
