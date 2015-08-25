523c4f8233ce6bc85f35487f140086f2b21539d5
Eric Leung <etleung21@gmail.com>
2015-08-24 23:26:48 -0500

    start exercise 9 for C programming

---
+++ c-lcthw/bin/ex9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int numbers[4] = {0}; 
+    char name[4] = {'a'};
+
+    // first, print them out raw
+    printf("numbers: %d %d %d %d\n",
+            number[0], number[1],
+            number[2], number[3]);
+    printf("name each: %c %c %c %c\n",
+            name[0], name[1],
+            name[2], name[3]);
+}
