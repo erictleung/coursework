a5973c622b487da9659fc89e312fa22f594aa441
Eric Leung <etleung21@gmail.com>
2015-08-27 21:07:56 -0500

    work on exercise 9; there's a lint error

--- c-lcthw/bin/ex9.c
+++ c-lcthw/bin/ex9.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    int numbers[4] = {0}; 
+    int number[4] = {0}; 
     char name[4] = {'a'};
 
     // first, print them out raw
@@ -12,4 +12,29 @@ int main(int argc, char *argv[])
     printf("name each: %c %c %c %c\n",
             name[0], name[1],
             name[2], name[3]);
+    printf("name: %s\n", name);
+
+    // setup the numbers
+    number[0] = 1;
+    number[1] = 2;
+    number[2] = 3;
+    number[3] = 4;
+    
+    // setup the name
+    name[0] = "Z";
+    name[1] = "e";
+    name[2] = "d";
+    name[3] = "\0";
+
+    // then print them out initialized
+    printf("numbers: %d %d %d %d\n",
+            number[0], number[1],
+            number[2], number[3]);
+
+    printf("name each: %c %c %c %c\n",
+            name[0], name[1],
+            name[2], name[3]);
+
+    // print the name like a string
+    printf("name: %s\n", name);
 }
