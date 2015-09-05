9f1e19f7b4828c9587c1c1f34e67fa5cc3b1f0cf
Eric Leung <etleung21@gmail.com>
2015-09-05 07:15:07 -0700

    correctly manage private and shared variables for pi program

--- openmp/bin/pi.c
+++ openmp/bin/pi.c
@@ -5,12 +5,12 @@ static long num_steps = 100000;
 double step;
 int main()
 {
-    double pi, sum = 0.0;
+    double x, pi, sum, bigSum;
     double start_time, run_time;
     step = 1.0/(double) num_steps;
-    int threads = 5;
+    int ID, start, end, i, l, threads = 5;
     int thr_len = num_steps/threads;
-    //double sum_thr[threads]; // each thread has own variable
+    double sum_thr[threads]; // each thread has own variable
 
     printf("Here are some parameters set:\n");
     printf("Step Size: %lf\n", step);
@@ -21,42 +21,30 @@ int main()
 
     start_time = omp_get_wtime();
 
-    #pragma omp parallel for private(ID,x_temp,temp_sum,start,end,i) shared(sum,step,thr_len)
+    #pragma omp parallel private(ID,x,sum,start,end,i) shared(step,thr_len,sum_thr)
     {
-    int ID = omp_get_thread_num(); // initialize thread ID
-    double x_temp = 0.0, temp_sum = 0.0; // temp sum for particular thread
-    int start = thr_len*ID; // start index
-    int end = thr_len*(ID+1); // end index
+    ID = omp_get_thread_num(); // initialize thread ID
+    start = thr_len*ID; // start index
+    end = thr_len*(ID+1); // end index
 
-    for (int i=start; i<end; i++) 
+    for (i=start; i<end; i++) 
     {
-        x_temp = (i+0.5)*step;
-        temp_sum = temp_sum + 4.0/(1.0+x_temp*x_temp);
+        x = (i+0.5)*step;
+        sum = sum + 4.0/(1.0+x*x);
     }
-    //sum_thr[ID] = temp_sum;
-    sum = temp_sum;
+    sum_thr[ID] = sum;
     }
 
-    // check array summing
-    //printf("Here are the values of the thread sums.\n");
-    //for (i=0; i<threads; i++) 
-    //{
-        //printf("%lf\n", sum_thr[i]);
-    //}
-    //printf("\n");
-    
-    // loop through sum of each thread and add them together
-    //for (i=0; i<threads; i++) 
-    //{
-     //   printf("This is the thread %d sum: %lf\n", i,sum_thr[i]);
-      //  printf("This is the sum so far: %lf\n", sum);
-       // sum = sum + sum_thr[i];
-        //printf("This is the sum after adding: %lf\n\n", sum);
-    //}
-    //printf("This is the sum: %lf\n", sum);
+    for (l=0; l<threads; l++) 
+    {
+        printf("Thread %d has sum: %lf\n", l, sum_thr[l]);
+        bigSum = bigSum + sum_thr[l];
+    }
+    printf("\n");
 
-    pi = step*sum;
+    pi = step*bigSum;
 
     run_time = omp_get_wtime() - start_time;
+    printf("Total sum is %lf\n", bigSum);
     printf("pi with %ld steps is %lf in %lf seconds.\n", num_steps,pi,run_time);
 }
