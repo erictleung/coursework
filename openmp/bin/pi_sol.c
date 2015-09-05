7a3e1026a912571efe536580f61a143909e36551
Eric Leung <etleung21@gmail.com>
2015-09-05 07:56:32 -0700

    add comments to Tim's solution to pi problem

--- openmp/bin/pi_sol.c
+++ openmp/bin/pi_sol.c
@@ -12,17 +12,23 @@ void main()
 
     #pragma omp parallel
     {
+    // initialize local variables for each thread
     int i, id, nthrds;
         double x;
         id = omp_get_thread_num();
         nthrds = omp_get_num_threads();
-        if (id == 0) nthreads = nthrds;
+
+    // save num threads in case computer doesn't give you all you requested
+    if (id == 0) nthreads = nthrds;
+
+    // cyclic distribution of loop interations
     for (i=id,sum[id]=0.0; i<num_steps; i=i+nthrds) 
     {
         x = (i+0.5)*step;
         sum[id] += 4.0/(1.0 + x*x);
     }
     }
+
     for (i=0,pi=0.0; i<nthreads; i++) pi += sum[i] * step;
     printf("%lf\n", pi);
 }
