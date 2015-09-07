60314759e51c870018667427c9f09a8693a2732a
Eric Leung <etleung21@gmail.com>
2015-09-07 00:34:19 -0700

    add interactive scripting to use grep

--- applied-bioinformatics/scripts/data-mani.sh
+++ applied-bioinformatics/scripts/data-mani.sh
@@ -8,14 +8,14 @@ if [ -f $FILE ];
 then
     echo "File is already downloaded. Let us continue."
 else
-    curl http://downloads.yeastgenome.org/curation/chromosomal_feature/saccharomyces_cerevisiae.gff -o ../data/sc.gff
+    curl http://downloads.yeastgenome.org/curation/chromosomal_feature/saccharomyces_cerevisiae.gff -o $FILE
     echo "Done downloading yeast genome for Saccharomyces cerevisiae."
 fi
 echo "We now have the genome we want to study.\n"
 
 # find out how many lines this file has
-read -p "Let us find how many lines are in this file. \
-(Press enter to continue) " NULL
+echo "Let us find how many lines are in this file."
+read -p "(Press enter to continue)" NULL
 echo "> wc -l ../data/sc.gff"
 wc -l ../data/sc.gff
 echo ""
@@ -25,3 +25,17 @@ echo "Let's look at just the beginning of the file to see what it looks like."
 read -p "(Press enter to continue)" NULL
 echo "> head ../data/sc.gff"
 head ../data/sc.gff
+echo ""
+
+# search for some simple patterns
+echo "Let's find the simple pattern of 'YAL060W' in our file."
+read -p "(Press enter to continue)" NULL
+echo "> grep YAL060W ../data/sc.gff"
+grep YAL060W ../data/sc.gff
+echo ""
+
+# how many lines match the word gene using piping/chaining commands
+echo "Let's see how many times the word 'gene' appears in the file."
+read -p "(Press enter to continue)" NULL
+echo "> grep gene ../data/sc.gff | wc -l"
+grep gene ../data/sc.gff | wc -l
